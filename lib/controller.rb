d7bd677730bdf3b877a2452ef4eaf51c02b1846a
Karol Lipski <karol.lipski@gmail.com>
2016-11-09 11:08:01 +0100

    slim integration

--- lib/controller.rb
+++ lib/controller.rb
@@ -11,7 +11,7 @@ class Controller
     send(action)
     self.status = 200
     self.headers = { "Content-Type" => 'text/html'}
-    self.content = ['This is content from Controller']
+    self.content = [template.render(self)]
     self
   end
 
@@ -27,5 +27,9 @@ class Controller
     self.headers = { }
     self.content = ['internal error']
     self
+  end
+
+  def template
+    Slim::Template.new(File.join(App.root,'app','views',"#{self.name}", "#{self.action}.slim"))
   end
 end
